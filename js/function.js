95dfe801cafcf5441c41c76b3d7a891567e80a18
sakashita-hitomi <sakashita.0604@gmail.com>
2024-10-01 23:41:28 +0900

    ファイルをすべて追加

---
+++ js/function.js
@@ -0,0 +1,17 @@
+$(function() {
+// ここにjQueryを記述
+
+//バーガーボタンをクリックしたら…
+$('#header__btn').on('click', function(){
+    //span要素にクラスを追加・削除
+     $('#header__btn__top').toggleClass('rotateTop');//上の線
+     $('#header__btn__middle').toggleClass('hideMiddle');//中央の線
+     $('#header__btn__bottom').toggleClass('rotateBottom');//下の線
+     //ナビゲーションをスライド表示させる
+ $('#gnav').toggleClass('translateNav');
+ });
+
+
+
+
+});
