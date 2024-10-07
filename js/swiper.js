e92ccd14fdb839f7cbeb4230fcd8edfc31668e07
sakashita-hitomi <sakashita.0604@gmail.com>
2024-10-07 16:31:38 +0900

    pc版レスポンシブmvまで

--- js/swiper.js
+++ js/swiper.js
@@ -4,11 +4,14 @@ const swiperWork = new Swiper('.swiper-work',{
     effect: 'slide',
     slidesPerView: 1.3,
     centeredSlides: true,
-    speaceBetween: 10,
+    spaceBetween: 10,
     slideActiveClass: 'swiper-work__Active',
     speed: 1000,
     autoplay: {
         delay: 3000,
         disableOnInterraction: true,
+        // @media screen and (min-width: 768px){
+        //     slidesPerView:2.3,
+        // }
     }
 });
