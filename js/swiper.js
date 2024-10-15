eb1a5337f9f99d13d891dbc3889500649886131f
sakashita-hitomi <sakashita.0604@gmail.com>
2024-10-15 16:39:30 +0900

    サーバーアップ後微調整、サンクスページ完成

--- js/swiper.js
+++ js/swiper.js
@@ -1,26 +1,26 @@
-const swiperWork = new Swiper('.swiper-work',{
-    direction: 'horizontal',
-    loop: true,
-    effect: 'slide',
-    slidesPerView: 1.3,
-    centeredSlides: true,
-    spaceBetween: 20,
-    slideActiveClass: 'swiper-work__Active',
-    speed: 1000,
-    autoplay: {
-        delay: 3000,
-        disableOnInterraction: true,
+const swiperWork = new Swiper('.swiper-work', {
+  direction: 'horizontal',
+  loop: true,
+  effect: 'slide',
+  slidesPerView: 1.3,
+  centeredSlides: true,
+  spaceBetween: 20,
+  slideActiveClass: 'swiper-work__Active',
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    disableOnInterraction: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
     },
-    breakpoints: {
-        768: {
-          slidesPerView: 2.5,
-          spaceBetween: 30,
-        }
-      },
-      breakpoints: {
-        1024: {
-          slidesPerView: 3.2,
-          spaceBetween: 50,
-        }
-      },
+    
+    1024: {
+      slidesPerView: 3.2,
+      spaceBetween: 50,
+    }
+  },
+ 
 });
